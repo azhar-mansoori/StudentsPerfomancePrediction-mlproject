9b61e8c969cfd50a00e7146d6b07b7043eb3b509
azhar-mansoori <azharuddin.10121995@gmail.com>
2024-10-22 04:30:14 +0530

    b

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from src.mlproject.logger import logging
+from src.mlproject.logger import logging 
+from src.mlproject.exception import CustomException
+import sys
 
 
 
@@ -8,3 +10,11 @@ from src.mlproject.logger import logging
 
 if __name__== "__main__":
     logging.info("the execution has strated")
+
+
+
+    try:
+        a=1/0
+    except Exception as e :
+         logging.info("custom exception")
+         raise CustomException(e,sys)    
